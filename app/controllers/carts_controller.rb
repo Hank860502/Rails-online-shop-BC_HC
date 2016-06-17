1ed83f9403dd5844ce61ccf5e8c7c30cf124e209
dbcbrianchung <brianjaychung@gmail.com>
2016-06-17 12:30:09 -0700

    add to shopping cart works

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,10 @@ class CartsController < ApplicationController
 
   def index
     @user = current_user
-    @product = params[:product]
+    @product = Product.find(params[:product])
+    @user.shoppingcart.push(@product)
+    @user.save
+    ## will be ajaxified, allowing user to click the button multiple times to push the item object into the shoppping cart array.
+
   end
 end
