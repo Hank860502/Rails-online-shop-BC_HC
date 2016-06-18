6aaad16103d38277614493bfe907e16db7c5df9b
dbcbrianchung <brianjaychung@gmail.com>
2016-06-17 17:07:34 -0700

    add ajax

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   $(".cart").on("click", function(event){
     event.preventDefault();
+    debugger
+    var url = $(this).children().attr("href")
+    $.ajax({
+      url: url,
+      type: 'GET',
+    }).done(function(data){
+      debugger;
+    })
   });
 });
